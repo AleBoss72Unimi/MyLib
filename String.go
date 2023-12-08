6877960b2a4e7f81f8ffb2b7fe349c6911ca7605
unknown <alessandro.bosi1@studenti.unimi.it>
2023-12-08 13:14:27 +0100

    v.1.0.2

--- String.go
+++ String.go
@@ -1,1 +1,27 @@
 package mylib
+
+import "strings"
+import "strconv"
+
+type String struct {
+	Str string
+}
+
+func (s String) ToInt() int {
+	i, _ := strconv.Atoi(s.Str)
+	return i
+}
+
+func (s String) ToFloat() float64 {
+	f, _ := strconv.ParseFloat(s.Str, 64)
+	return f
+}
+
+func (s String) ToBool() bool {
+	b, _ := strconv.ParseBool(s.Str)
+	return b
+}
+
+func (s String) ReplaceAll(old, new string) string {
+	return strings.ReplaceAll(s.Str, old, new)
+}
